fdbd4134db81657172cec21377752f0aff60b9de
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-10-22 11:29:32 +0200

    fix: remove hopefully unnecessary tabs permission and added active switch to loop

--- background.js
+++ background.js
@@ -2,10 +2,12 @@ async function openStartupTabs(){
 	const res = await browser.storage.local.get('startup-tabs');
 	if ( Array.isArray(res['startup-tabs']) ){
 		for(const obj of res['startup-tabs']) {
-			browser.tabs.create({
-				'url': obj.url,
-				'active': false
-			});
+			if(obj.activ) {
+				browser.tabs.create({
+					'url': obj.url,
+					'active': false
+				});
+			}
 		}
 	}
 }
