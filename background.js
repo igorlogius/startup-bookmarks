5681fa2d747dd7739eeaa241faa0c486ddde217a
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-10-26 15:18:14 +0200

    feat: use bookmark folder instead of an option page

--- background.js
+++ background.js
@@ -1,16 +1,63 @@
+//const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
+const manifest = browser.runtime.getManifest();
+const extname = manifest.name;
+
 async function openStartupTabs(){
-	const res = await browser.storage.local.get('startup-tabs');
-	if ( Array.isArray(res['startup-tabs']) ){
-		for(const obj of res['startup-tabs']) {
-			if(obj.activ) {
-				browser.tabs.create({
-					'url': obj.url,
-					'active': false
-				});
-			}
+	try {
+		let tmp = await browser.storage.local.get(extname);
+		if (!tmp) { return; }
+		tmp = tmp[extname];
+		if (!tmp) { return; }
+		const urls = (await browser.bookmarks.getChildren(tmp))
+			.filter( child => child.url) // ignore sub folders
+			.map( child => child.url);
+		let first = true;
+		for(const url of urls) {
+			browser.tabs.create({
+				'url': url,
+				'active': first
+			});
+			first = false;
 		}
+	}catch(e){
+		console.error(e);
 	}
 }
 
 browser.runtime.onStartup.addListener(openStartupTabs);
-browser.browserAction.onClicked.addListener(openStartupTabs);
+
+browser.menus.create({
+	id: extname,
+	title: 'Open at Startup', 
+	type: "radio",
+	contexts: ["bookmark"],
+	visible: false,
+	checked: false,
+	onclick: async function(info, tab) {
+		if(info.bookmarkId ) {
+			//blub = { 'startup-tabs': info.bookmarkId }
+
+			let tmp = await browser.storage.local.get(extname);
+			if (tmp) {
+				tmp = tmp[extname];
+			}
+			let blub = {};
+			blub[extname] = (info.bookmarkId === tmp)? undefined: info.bookmarkId;
+			browser.storage.local.set(blub);
+		}
+	}
+});
+
+browser.menus.onShown.addListener(async function(info, tab) {
+	if(info.bookmarkId ) {
+		const bmn = (await browser.bookmarks.get(info.bookmarkId))[0];
+		if(!bmn.url) {
+			let tmp = await browser.storage.local.get(extname);
+			if (tmp) { tmp = tmp[extname]; }
+			browser.menus.update(extname, {visible: true, checked: (tmp === info.bookmarkId)});
+		}else{
+			browser.menus.update(extname, {visible: false, checked: false});
+		}
+	}
+	browser.menus.refresh();
+});
