2f9f9aec3a94abffad2206c56d0dece400ce4a5d
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-09-05 14:04:35 +0200

    feat(#1): adds option to open tabs in a new window

--- background.js
+++ background.js
@@ -3,27 +3,46 @@
 const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 
+async function getFromStorage(type, id, fallback) {
+    let tmp = await browser.storage.local.get(id);
+    return (typeof tmp[id] === type) ? tmp[id] : fallback;
+}
+
 async function openStartupTabs(){
-		let tmp = await browser.storage.local.get(extname);
-		if (!tmp) { return; }
-		tmp = tmp[extname];
-		if (!tmp) { return; }
 
-        let already_open_urls = new Set((await browser.tabs.query({})).filter( t => t.url ).map( t => t.url));
+    const bmId = await getFromStorage('string', extname, undefined);
+    if(!bmId){
+        console.log(extname, 'no folder in storage');
+        return;
+    }
+    const openInNewWindow= await getFromStorage('boolean', 'window', false);
 
-		const urls = new Set((await browser.bookmarks.getChildren(tmp))
-			.filter( child => child.url) // ignore sub folders
-			.filter( child => !already_open_urls.has(child.url)) // ignore sub folders
-			.map( child => child.url));
+    //let already_open_urls = new Set((await browser.tabs.query({})).filter( t => t.url ).map( t => t.url));
 
-		let first = true;
+    const urls = new Set((await browser.bookmarks.getChildren(bmId))
+            .filter( child => child.url) // ignore sub folders
+            //.filter( child => !already_open_urls.has(child.url)) // ignore sub folders
+            .map( child => child.url));
+
+    console.log(urls);
+
+    if(openInNewWindow){
+        const titlePreface = (await getFromStorage('string', 'titlePreface', extname)) + " : " ;
+
+        browser.windows.create({
+            url: [...urls],
+            titlePreface
+        });
+    }else{
+        let first = true;
         for(const url of urls) {
-                browser.tabs.create({
-                        'url': url,
-                        'active': first
-                        });
-                first = false;
+            browser.tabs.create({
+                    'url': url,
+                    'active': first
+                    });
+            first = false;
         }
+    }
 }
 
 browser.runtime.onStartup.addListener(openStartupTabs);
@@ -44,6 +63,7 @@ browser.menus.create({
 			let blub = {};
 			blub[extname] = (info.bookmarkId === tmp)? undefined: info.bookmarkId;
 			browser.storage.local.set(blub);
+            console.log(extname, ' folder selected');
 		}
 	}
 });
@@ -62,4 +82,16 @@ browser.menus.onShown.addListener(async function(info/*, tab*/) {
 	browser.menus.refresh();
 });
 
-browser.browserAction.onClicked.addListener(openStartupTabs);
+//browser.browserAction.onClicked.addListener(openStartupTabs);
+browser.browserAction.onClicked.addListener((tab, data) => {
+
+            if(data.button === 1){
+                openStartupTabs();
+                return;
+            }
+            browser.tabs.create({
+                    'url': "options.html",
+                    'active': true
+                    });
+});
+
