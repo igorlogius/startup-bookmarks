94a7a42b7696b50a19fb4b1b40c463d75114215a
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-01-17 00:10:29 +0100

    fix: remove inital newtab without Open Window

--- background.js
+++ background.js
@@ -4,102 +4,65 @@ const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 
 async function getFromStorage(type, id, fallback) {
-    let tmp = await browser.storage.local.get(id);
-    return (typeof tmp[id] === type) ? tmp[id] : fallback;
+	let tmp = await browser.storage.local.get(id);
+	return (typeof tmp[id] === type) ? tmp[id] : fallback;
 }
 
 async function setToStorage(id, value) {
-    let obj = {};
-    obj[id] = value
-    return browser.storage.local.set(obj);
+	let obj = {};
+	obj[id] = value
+	return browser.storage.local.set(obj);
 }
 
 async function openStartupTabs(){
 
-    let tmp;
-    const bmId = await getFromStorage('string', 'folder', undefined);
-    if(!bmId){
-        return;
-    }
-    const openInNewWindow= await getFromStorage('boolean', 'window', false);
+	let tmp;
+	const bmId = await getFromStorage('string', 'folder', '');
+	if(bmId === ''){
+		return;
+	}
 
-    //let already_open_urls = new Set((await browser.tabs.query({})).filter( t => t.url ).map( t => t.url));
+	const urls = new Set((await browser.bookmarks.getChildren(bmId))
+		.filter( child => child.url) // ignore sub folders
+		.map( child => child.url));
 
-    //try {
-        const urls = new Set((await browser.bookmarks.getChildren(bmId))
-                .filter( child => child.url) // ignore sub folders
-                //.filter( child => !already_open_urls.has(child.url)) // ignore sub folders
-                .map( child => child.url));
+	if(urls.size < 1) {
+		return;
+	}
 
-        if(urls.size< 1) {
-            return;
-        }
-        if(openInNewWindow){
-            const titlePreface = (await getFromStorage('string', 'titlePreface', extname)) + " : " ;
+	const openInNewWindow = await getFromStorage('boolean', 'window', true);
 
-            const win = await browser.windows.create({
-                titlePreface
-            });
+	if(openInNewWindow){
+		const titlePreface = (await getFromStorage('string', 'titlePreface', extname)) + " : " ;
+		tmp = await browser.windows.create({ titlePreface });
+	}else{
+		tmp = (await browser.windows.getCurrent({ populate: false }));
+	}
+	const winId = tmp.id;
 
-	    const createdTabIds = new Set();
+	const createdTabIds = new Set();
 
-            let first = true;
-            for(const url of urls) {
-                tmp = await browser.tabs.create({
-			'windowId': win.id,
+	let first = true;
+	for(const url of urls) {
+		tmp = await browser.tabs.create({
+			'windowId': winId,
 			'pinned': url.endsWith('#pin'),
-                        'url': url,
-                        'active': first
-                        });
-                first = false;
+			'url': url,
+			'active': first
+		});
+		first = false;
 		createdTabIds.add(tmp.id);
-            }
-	    // remove the inital about:newtab and everything else not part of the startup tabs
-	    const itabIds = (await browser.tabs.query({windowId: win.id})).filter(t => !createdTabIds.has(t.id)).map(t => t.id);
-	    browser.tabs.remove(itabIds);
-        }else{
-            let first = true;
-            for(const url of urls) {
-                browser.tabs.create({
-			'pinned': url.endsWith('#pin'),
-                        'url': url,
-                        'active': first
-                        });
-                first = false;
-            }
-        }
-    /*}catch(e){
-        console.error(e);
-    }*/
+	}
+	// remove the inital about:newtab and everything else not part of the startup tabs
+	const itabIds = (await browser.tabs.query({windowId: winId})).filter(t => !createdTabIds.has(t.id)).map(t => t.id);
+	browser.tabs.remove(itabIds);
 }
 
 browser.runtime.onStartup.addListener(openStartupTabs);
-
-/*
-browser.browserAction.onClicked.addListener( () => {
-    browser.windows.create({
-        url: ["dialog.html"],
-        type: "popup",
-        width: 300,
-        height: 250
-    });
-});
-*/
 
 browser.runtime.onMessage.addListener( (req /*,sender, sendRes*/) => {
         if(req.cmd === 'testStartupTabs') {
             openStartupTabs();
         }
 });
-
-async function onInstalled(details) {
-    if(details.reason === 'update') {
-        let tmp = await getFromStorage('string',extname, '');
-        if(tmp !== ''){
-            setToStorage('folder',tmp);
-        }
-    }
-}
-
-browser.runtime.onInstalled.addListener(onInstalled);
 
