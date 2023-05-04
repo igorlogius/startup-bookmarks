6c153246661ac90b9f9d4a502c8b707a65f76b62
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-05-04 11:41:35 +0200

    feat(#2): prevent some duplicate creations

--- background.js
+++ background.js
@@ -4,59 +4,71 @@ const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 
 async function getFromStorage(type, id, fallback) {
-	let tmp = await browser.storage.local.get(id);
-	return (typeof tmp[id] === type) ? tmp[id] : fallback;
+  let tmp = await browser.storage.local.get(id);
+  return typeof tmp[id] === type ? tmp[id] : fallback;
 }
 
-async function openStartupTabs(){
+async function openStartupTabs() {
+  let tmp;
+  const bmId = await getFromStorage("string", "folder", "");
+  if (bmId === "") {
+    return;
+  }
 
-	let tmp;
-	const bmId = await getFromStorage('string', 'folder', '');
-	if(bmId === ''){
-		return;
-	}
+  const urls = new Set(
+    (await browser.bookmarks.getChildren(bmId))
+      .filter((child) => child.url) // ignore sub folders
+      .map((child) => child.url)
+  );
 
-	const urls = new Set((await browser.bookmarks.getChildren(bmId))
-		.filter( child => child.url) // ignore sub folders
-		.map( child => child.url));
+  if (urls.size < 1) {
+    return;
+  }
 
-	if(urls.size < 1) {
-		return;
-	}
+  let already_open_urls = new Set();
 
-	const openInNewWindow = await getFromStorage('boolean', 'window', false);
+  const openInNewWindow = await getFromStorage("boolean", "window", false);
 
-	if(openInNewWindow){
-		const titlePreface = (await getFromStorage('string', 'titlePreface', extname)) + " : " ;
-		tmp = await browser.windows.create({ titlePreface });
-	}else{
-		tmp = (await browser.windows.getCurrent({ populate: false }));
-	}
-	const winId = tmp.id;
+  if (openInNewWindow) {
+    const titlePreface =
+      (await getFromStorage("string", "titlePreface", extname)) + " : ";
+    tmp = await browser.windows.create({ titlePreface });
+  } else {
+    tmp = await browser.windows.getCurrent({ populate: false });
+    already_open_urls = new Set(
+      (await browser.tabs.query({})).map((t) => t.url)
+    );
+  }
+  console.debug(already_open_urls);
+  const winId = tmp.id;
 
-	const createdTabIds = new Set();
+  const createdTabIds = new Set();
 
-	let first = true;
-	for(const url of urls) {
-		tmp = await browser.tabs.create({
-			'windowId': winId,
-			'pinned': url.endsWith('#pin'),
-			'url': url,
-			'active': first
-		});
-		first = false;
-		createdTabIds.add(tmp.id);
-	}
-	// remove  none http
-	const itabIds = (await browser.tabs.query({windowId: winId})).filter(t => !(/^https?:/.test(t.url) || createdTabIds.has(t.id))).map(t => t.id);
-	browser.tabs.remove(itabIds);
+  let first = true;
+  for (const url of urls) {
+    if (!already_open_urls.has(url)) {
+      console.debug(url, " not in ", already_open_urls);
+      tmp = await browser.tabs.create({
+        windowId: winId,
+        pinned: url.endsWith("#pin"),
+        url: url,
+        active: first,
+      });
+      first = false;
+      createdTabIds.add(tmp.id);
+    }
+  }
+  // remove  none http
+  const itabIds = (await browser.tabs.query({ windowId: winId }))
+    .filter((t) => !(/^https?:/.test(t.url) || createdTabIds.has(t.id)))
+    .map((t) => t.id);
+  browser.tabs.remove(itabIds);
 }
 
 browser.runtime.onStartup.addListener(openStartupTabs);
 
-browser.runtime.onMessage.addListener( (req /*,sender, sendRes*/) => {
-        if(req.cmd === 'testStartupTabs') {
-            openStartupTabs();
-        }
+browser.runtime.onMessage.addListener((req /*,sender, sendRes*/) => {
+  if (req.cmd === "testStartupTabs") {
+    openStartupTabs();
+  }
 });
-
--- dialog.js
+++ dialog.js
@@ -1,131 +1,124 @@
 /* global browser */
 
-const folders = document.getElementById('folder');
-const testbtn = document.getElementById('test');
+const folders = document.getElementById("folder");
+const testbtn = document.getElementById("test");
 
 async function getFromStorage(type, id, fallback) {
-    let tmp = await browser.storage.local.get(id);
-    return (typeof tmp[id] === type) ? tmp[id] : fallback;
+  let tmp = await browser.storage.local.get(id);
+  return typeof tmp[id] === type ? tmp[id] : fallback;
 }
 
+/*
 async function setToStorage(id, value) {
-    let obj = {};
-    obj[id] = value
-    return browser.storage.local.set(obj);
+  let obj = {};
+  obj[id] = value;
+  return browser.storage.local.set(obj);
 }
-
+*/
 
 function onChange(evt) {
+  let id = evt.target.id;
+  let el = document.getElementById(id);
 
-	let id = evt.target.id;
-	let el = document.getElementById(id);
+  let value = el.type === "checkbox" ? el.checked : el.value;
+  let obj = {};
 
-	let value = ( (el.type === 'checkbox') ? el.checked : el.value)
-	let obj = {}
+  if (el.type === "number") {
+    try {
+      value = parseInt(value);
+      if (isNaN(value)) {
+        value = el.min;
+      }
+      if (value < el.min) {
+        value = el.min;
+      }
+    } catch (e) {
+      value = el.min;
+    }
+  }
 
-	if(el.type === 'number'){
-		try {
-			value = parseInt(value);
-			if(isNaN(value)){
-				value = el.min;
-			}
-			if(value < el.min) {
-				value = el.min;
-			}
-		}catch(e){
-			value = el.min
-		}
-	}
+  obj[id] = value;
 
-	obj[id] = value;
-
-	browser.storage.local.set(obj).catch(console.error);
-
+  browser.storage.local.set(obj).catch(console.error);
 }
 
-[  "folder", "window", "titlePreface" ].map( (id) => {
+["folder", "window", "titlePreface"].map((id) => {
+  browser.storage.local
+    .get(id)
+    .then((obj) => {
+      let el = document.getElementById(id);
+      let val = obj[id];
 
-	browser.storage.local.get(id).then( (obj) => {
+      console.log(id, val);
 
-		let el = document.getElementById(id);
-		let val = obj[id];
+      if (typeof val !== "undefined") {
+        if (el.type === "checkbox") {
+          el.checked = val;
+        } else {
+          el.value = val;
+        }
+      }
+    })
+    .catch(console.error);
 
-        console.log(id,val);
-
-		if(typeof val !== 'undefined') {
-			if(el.type === 'checkbox') {
-				el.checked = val;
-			}
-			else{
-				el.value = val;
-			}
-		}
-
-	}).catch(console.error);
-
-	let el = document.getElementById(id);
-	el.addEventListener('input', onChange);
+  let el = document.getElementById(id);
+  el.addEventListener("input", onChange);
 });
 
-
-function recGetFolders(node, depth = 0){
-    let out = new Map();
-    if(typeof node.url !== 'string'){
-        if(node.id !== 'root________'){
-            out.set(node.id, { 'depth': depth, 'title': node.title });
-        }
-        if(node.children){
-            for(let child of node.children){
-                out = new Map([...out, ...recGetFolders(child, depth+1) ]);
-            }
-        }
+function recGetFolders(node, depth = 0) {
+  let out = new Map();
+  if (typeof node.url !== "string") {
+    if (node.id !== "root________") {
+      out.set(node.id, { depth: depth, title: node.title });
     }
-    return out;
+    if (node.children) {
+      for (let child of node.children) {
+        out = new Map([...out, ...recGetFolders(child, depth + 1)]);
+      }
+    }
+  }
+  return out;
 }
 
 async function initSelect() {
-    const nodes = await browser.bookmarks.getTree();
-    let out = new Map();
-    let depth = 1;
-    for(const node of nodes){
-        out = new Map([...out, ...recGetFolders(node, depth) ]);
+  const nodes = await browser.bookmarks.getTree();
+  let out = new Map();
+  let depth = 1;
+  for (const node of nodes) {
+    out = new Map([...out, ...recGetFolders(node, depth)]);
+  }
+  let tmp = await getFromStorage("string", "folder", "");
+  let last_val = "";
+  for (const [k, v] of out) {
+    //console.debug(k, v.title);
+    folders.add(new Option("-".repeat(v.depth) + " " + v.title, k));
+    if (k === tmp) {
+      last_val = k;
     }
-    let tmp = await getFromStorage('string','folder','')
-    let last_val = '';
-    for(const [k,v] of out){
-        //console.debug(k, v.title);
-        folders.add(new Option("-".repeat(v.depth) + " " + v.title, k))
-        if(k === tmp){
-            last_val = k;
-        }
-    }
-    folders.value = last_val;
+  }
+  folders.value = last_val;
 }
 
 async function onLoad() {
+  await initSelect();
 
-    await initSelect();
+  if (folders.value !== "") {
+    testbtn.disabled = false;
+  } else {
+    testbtn.disabled = true;
+  }
 
-        if(folders.value !== ''){
-            testbtn.disabled = false;
-        }else{
-            testbtn.disabled = true;
-        }
-
-
-    folders.addEventListener('input', function () {
-        if(folders.value !== ''){
-            testbtn.disabled = false;
-        }else{
-            testbtn.disabled = true;
-        }
-    });
-    testbtn.addEventListener('click', function () {
-        // send msg to background script
-        browser.runtime.sendMessage({ cmd: "testStartupTabs" })
-    });
-
+  folders.addEventListener("input", function () {
+    if (folders.value !== "") {
+      testbtn.disabled = false;
+    } else {
+      testbtn.disabled = true;
+    }
+  });
+  testbtn.addEventListener("click", function () {
+    // send msg to background script
+    browser.runtime.sendMessage({ cmd: "testStartupTabs" });
+  });
 }
 
-document.addEventListener('DOMContentLoaded', onLoad);
-
+document.addEventListener("DOMContentLoaded", onLoad);
